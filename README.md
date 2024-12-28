ff94764ac484f326802e9449546b9b40cde12289
Alexander Korolev <alexander.korolev.germany@gmail.com>
2024-12-28 18:23:43 +0100

    llm-ollama

---
+++ README.md
@@ -0,0 +1,16 @@
+# llm-ollama description
+
+## Legal
+
+Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+llm-ollama = "0.1"
+```
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/*!
+# llm-ollama description
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+llm-ollama = "0.1"
+```
+
+*/
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
